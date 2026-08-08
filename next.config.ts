815ee0f0375961bf7127c6f657f22877e042b3c9
thepatestate <thepatestate@users.noreply.github.com>
2026-08-07 14:30:36 -1000

    feat: scaffold Next.js app

---
+++ next.config.ts
@@ -0,0 +1,7 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: { remotePatterns: [{ protocol: "https", hostname: "*.ytimg.com" }] },
+};
+
+export default nextConfig;
